24b61079f6eb0f27a159f79c1eb295d5a468bb3a
Châu Ngọc Sơn <ngocson877469@gmail.com>
2026-08-03 14:03:14 +0700

    buiuld

--- igourmet-internal/src/main.tsx
+++ igourmet-internal/src/main.tsx
@@ -5,6 +5,8 @@ import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './context/AuthContext.tsx'
 
+document.documentElement.classList.add('dark')
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
